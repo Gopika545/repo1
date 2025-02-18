8dc9c0de466423db56d1d8433141f965d7041772
AKHIL M S <AKHIL M S@DESKTOP-KKA2819>
2025-02-18 14:38:36 +0530

    initial commit

---
+++ mavenproject/src/main/java/mavenproject/Examples.java
@@ -0,0 +1,13 @@
+package mavenproject;
+
+public class Examples {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=6;
+		int c=a+b;
+		System.out.println(c);// TODO Auto-generated method stub
+
+	}
+
+}
